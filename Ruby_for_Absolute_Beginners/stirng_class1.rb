509fffbd281efa734392644bfba4dda5c2f126d9
Jim Hessin <jhessin@gmail.com>
2019-12-19 06:29:25 -0700

    Updated

---
+++ Ruby_for_Absolute_Beginners/stirng_class1.rb
@@ -0,0 +1,14 @@
+#!/bin/env ruby
+# frozen_string_literal:true
+
+name = 'John Doe'
+puts name
+puts name.class
+
+# address = '7843 California Avenue, New York, NY 10021'
+# puts address
+# puts address.class
+
+city = 'New York City'
+state = 'New York'
+print "#{city}, #{state}"
---
+++ Ruby_for_Absolute_Beginners/string_class2_methods.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+# String Methods
+
+# Concat - Appends the new string value with another string
+
+puts 'John'.concat 'Doe'
