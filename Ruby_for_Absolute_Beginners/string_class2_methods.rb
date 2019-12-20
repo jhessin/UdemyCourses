f2f6ee1a5fee67a911fa774890a6ec24b625f852
Jim Hessin <jhessin@gmail.com>
2019-12-19 21:24:18 -0700

    Updated

--- Ruby_for_Absolute_Beginners/string_class2_methods.rb
+++ Ruby_for_Absolute_Beginners/string_class2_methods.rb
@@ -1,7 +1,34 @@
-# frozen_string_literal: true
-
 # String Methods
+# frozen_string_literal:false
 
 # Concat - Appends the new string value with another string
 
-puts 'John'.concat 'Doe'
+puts 'John'.concat('Doe')
+first_name = 'Danny'
+last_name = ' Johnson'
+
+puts first_name.concat last_name
+
+# upcase - returns string Converted to uppercase
+
+city = 'Boston'
+
+puts city.upcase
+puts city
+puts '-------------------'
+puts city.upcase!
+puts city
+
+# downcase
+
+state = 'Nebraska'
+puts state.downcase
+puts state
+
+# delete
+
+country = 'Denmark'
+puts country.delete 'a'
+puts country.delete 'Da'
+country = 'United States'
+puts country.delete 'e'
