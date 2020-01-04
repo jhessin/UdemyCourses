36acda4bc88c1c8365b73b064193edf3a184837d
Jim Hessin <jhessin@gmail.com>
2020-01-04 15:55:41 -0700

    Update

---
+++ Complete_Ruby_on_Rails_Developer/25-numbers.rb
@@ -0,0 +1,17 @@
+#!/bin/env ruby
+# frozen_string_literal: false
+
+x = 5
+y = 10
+
+puts y / x
+
+puts 'I am a line'
+puts '-' * 20
+puts 'I am another line after a divider'
+
+20.times { print '-' }
+
+20.times { puts 'hi' }
+
+20.times { puts rand(10) }
